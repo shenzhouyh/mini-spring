0b15a0047a05c88fad7ba53fa56e93e64f4fc241
shenzhouyh <286817442@qq.com>
2019-10-14 19:21:31 +0800

    Initial commit

---
+++ framework/src/main/java/com/imooc/shenzhouyh/starter/MiniApplication.java
@@ -0,0 +1,15 @@
+package com.imooc.shenzhouyh.starter;
+
+/**
+ * @author: yinhao
+ * @email:286817442@qq.com
+ * @date:2019/10/14
+ * @time:11:34
+ * @desciption
+ */
+public class MiniApplication {
+
+  public static void run(Class<?> cls, String[] args) {
+    System.out.println("hello miniSpring");
+  }
+}
---
+++ test/src/main/java/com/imooc/shenzhouyh/Application.java
@@ -0,0 +1,17 @@
+package com.imooc.shenzhouyh;
+
+import com.imooc.shenzhouyh.starter.MiniApplication;
+
+/**
+ * @author: yinhao
+ * @email:286817442@qq.com
+ * @date:2019/10/14
+ * @time:11:21
+ * @desciption
+ */
+public class Application {
+  public static void main(String[] args) {
+    System.out.println("hello world!");
+    MiniApplication.run(Application.class,args);
+  }
+}
